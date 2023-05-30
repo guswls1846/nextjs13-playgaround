5e9ddda66e97317c609e681fd2b90cc2c92d1cea
oil <guswls1846@naver.com>
2023-05-30 12:22:13 +0900

    feat: dashboard, settings 페이지 추가

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            대시보드입니다.
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+            설정 페이지 입니다.
+        </div>
+    );
+};
+
+export default Settings;

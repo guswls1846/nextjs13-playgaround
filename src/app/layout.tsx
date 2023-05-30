b059afb32d252d8e9d415b07cd3189f3057fbb86
oil <guswls1846@naver.com>
2023-05-31 00:04:18 +0900

    feat: 루트 레이아웃 텍스트 표시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,14 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-yellow-700`}>
+        루트 레이아웃
+        {children}
+      </body>
     </html>
-  )
+  );
 }
